172cd7ddb66876743f916eb52513fea9ae53565a
Itsalok121 <141836103+Itsalok121@users.noreply.github.com>
2024-02-14 23:06:59 +0530

    Added problem statement

--- New Companies.sql
+++ New Companies.sql
@@ -1,3 +1,17 @@
+/*
+Problem Statement:
+    Amber's conglomerate corporation just acquired some new companies. Each of the companies follows this hierarchy:
+    Founder -> Lead Manager -> Senior Manager -> Manager -> Employee
+
+    Write a query to print the company_code, founder name, total number of lead managers, total number of senior managers, total number of managers, and total number of employees. Order your output by ascending company_code.
+    Note:
+        The tables may contain duplicate records.
+        The company_code is string, so the sorting should not be numeric. For example, if the company_codes are C_1, C_2, and C_10, then the ascending company_codes will be C_1, C_10, and C_2.
+    
+Hackerrank Link: 
+    https://www.hackerrank.com/challenges/the-company/problem
+*/
+
 select
     c.company_code,
     c.founder,
