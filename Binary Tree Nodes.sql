1d6eecafa5a5b80c1b5f585262b69fa7ed67d5fd
Itsalok121 <141836103+Itsalok121@users.noreply.github.com>
2024-02-14 23:03:02 +0530

    Added original problem statement.

--- Binary Tree Nodes.sql
+++ Binary Tree Nodes.sql
@@ -1,3 +1,15 @@
+/* Original Problem
+
+You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+
+Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+
+    Root: If node is root node.
+    Leaf: If node is leaf node.
+    Inner: If node is neither root nor leaf node.
+
+*/
+
 select 
     N,
     case 
@@ -17,6 +29,9 @@ select
                     'Leaf'
             end
         end as node
-from bst order by N
+from 
+    bst 
+order by
+    N;
             
         
